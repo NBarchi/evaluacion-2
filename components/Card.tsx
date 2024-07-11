6ba14b68c19f4c4f89ec5dbf17f446e28ca3fce0
NBarchi <nicobarrera.g10@gmail.com>
2024-07-10 21:50:12 -0500

    Commit

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,14 +5,24 @@ export default function Card(props:any) {
     console.log(props);
     
   return (
-    <View>
+    <View style={{marginTop:20}}>
         <Text>Orden: {props.data.id}</Text>
-        <Text>${props.data.monto}</Text>
+        <Text style={{fontWeight:'bold'}}>$ {props.data.monto}</Text>
         <Text>{props.data.comentario}</Text>
+        <View
+         style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            marginTop: 10,
+            marginRight:250
+         }}
+        />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    
+    container:{
+        marginTop:30
+    }
 })
--- screens/HistorialScreen.tsx
+++ screens/HistorialScreen.tsx
@@ -27,21 +27,42 @@ export default function HistorialScreen() {
   }, [])
   
   return (
-    <View>
-      <Text>Historial</Text>
+    <View style={styles.container}>
+      <Text style={{fontSize:20, marginLeft:30, color:'#473194' }}>Historial</Text>
+      <View
+         style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            margin:20
+         }}
+        />
+
       <View>
-        <Text>Mi Historial</Text>
+        <Text style={{fontSize:30, marginLeft:30, color:'#473194'}}>Mi Historial</Text>
       </View>
+      <View style={styles.container_Card}>
       <FlatList
         data={lista}
         renderItem={({item})=>
             <Card data={item}/>
         }
       />
-
+      </View>  
       
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop:100
+    },
+
+    container_Card:{
+        flex:1,
+        marginTop:20,
+        marginLeft:70
+        
+    }
+})
